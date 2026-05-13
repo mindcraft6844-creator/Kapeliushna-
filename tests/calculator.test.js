d4e8327e13849badb3eb0a92baf7b42221a5b01a
mindcraft6844-creator <mindcraft6844@gmail.com>
2026-05-13 14:25:05 +0000

    ci: add unit tests and CI pipeline

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,9 +1,15 @@
 const Calculator = require('../src/calculator');
 
-test('add: 2 + 3 should equal 5', () => {
-    expect(Calculator.add(2, 3)).toBe(5);
-});
+describe('Calculator Tests', () => {
+    test('add: 2 + 3 should equal 5', () => {
+        expect(Calculator.add(2, 3)).toBe(5);
+    });
 
-test('subtract: 5 - 3 should equal 2', () => {
-    expect(Calculator.subtract(5, 3)).toBe(2);
+    test('subtract: 5 - 3 should equal 2', () => {
+        expect(Calculator.subtract(5, 3)).toBe(2);
+    });
+
+    test('multiply: 4 * 3 should equal 12', () => {
+        expect(Calculator.multiply(4, 3)).toBe(12);
+    });
 });
