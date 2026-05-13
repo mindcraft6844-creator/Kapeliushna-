5f71c9ffce44a020e18877880a9f3e67fe700ea3
mindcraft6844-creator <mindcraft6844@gmail.com>
2026-05-13 14:28:15 +0000

    fix: correct broken test

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,5 +1,15 @@
 const Calculator = require('../src/calculator');
 
-test('add: 2 + 3 should equal 5 (THIS WILL FAIL)', () => {
-    expect(Calculator.add(2, 3)).toBe(99);  // 99 замість 5 - помилка!
+describe('Calculator Tests', () => {
+    test('add: 2 + 3 should equal 5', () => {
+        expect(Calculator.add(2, 3)).toBe(5);
+    });
+
+    test('subtract: 5 - 3 should equal 2', () => {
+        expect(Calculator.subtract(5, 3)).toBe(2);
+    });
+
+    test('multiply: 4 * 3 should equal 12', () => {
+        expect(Calculator.multiply(4, 3)).toBe(12);
+    });
 });
