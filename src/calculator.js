2887da7d37433f79ac2fe4e1649ddf6d6c8e522a
mindcraft6844-creator <mindcraft6844@gmail.com>
2026-05-13 13:30:02 +0000

    style: add debug logging to calculator

--- src/calculator.js
+++ src/calculator.js
@@ -1,22 +1,20 @@
 const Calculator = {
     add(a, b) {
+        console.log(`[DEBUG] Adding ${a} + ${b}`);
         return a + b;
     },
-    
     subtract(a, b) {
+        console.log(`[DEBUG] Subtracting ${a} - ${b}`);
         return a - b;
     },
-    
     multiply(a, b) {
+        console.log(`[DEBUG] Multiplying ${a} * ${b}`);
         return a * b;
     },
-    
     divide(a, b) {
-        if (b === 0) {
-            throw new Error('Division by zero');
-        }
+        if (b === 0) throw new Error('Division by zero');
+        console.log(`[DEBUG] Dividing ${a} / ${b}`);
         return a / b;
     }
 };
-
 module.exports = Calculator;
