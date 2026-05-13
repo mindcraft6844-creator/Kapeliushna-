e284d796b4469a5e0e02fe8fad855d2eabfe37b9
mindcraft6844-creator <mindcraft6844@gmail.com>
2026-05-13 14:25:55 +0000

    test: intentionally break unit test

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,15 +1,5 @@
 const Calculator = require('../src/calculator');
 
-describe('Calculator Tests', () => {
-    test('add: 2 + 3 should equal 5', () => {
-        expect(Calculator.add(2, 3)).toBe(5);
-    });
-
-    test('subtract: 5 - 3 should equal 2', () => {
-        expect(Calculator.subtract(5, 3)).toBe(2);
-    });
-
-    test('multiply: 4 * 3 should equal 12', () => {
-        expect(Calculator.multiply(4, 3)).toBe(12);
-    });
+test('add: 2 + 3 should equal 5 (THIS WILL FAIL)', () => {
+    expect(Calculator.add(2, 3)).toBe(99);  // 99 замість 5 - помилка!
 });
